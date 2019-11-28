c1e9d386f6b4e3397cc2218cebbb1c84f8a3697d
Eduardo Patrocinio <epatro@gmail.com>
2019-11-27 20:08:15 -0500

    Work on Test Resilience

--- test-resilience/server.js
+++ test-resilience/server.js
@@ -3,9 +3,10 @@ const MESSAGE_URL = "http://frontend.guestbook/guestbook.php?cmd=get&key=message
 var request = require('request')
 
 function countMessages () {
-	request.get(url, function (error, response, body) {
+	request.get(MESSAGE_URL, function (error, response, body) {
 		console.log('error:', error); // Print the error if one occurred
 		console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+		console.log ("Body: ", body)
 	})
 
 }
---
+++ test-resilience/testResilience.sh
@@ -0,0 +1,3 @@
+oc project guestbook-test
+oc delete -f testResilience.yaml
+oc create -f testResilience.yaml
