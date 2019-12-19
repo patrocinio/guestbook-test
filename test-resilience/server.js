cfa850a0e46dcfad478a14253fa4d4b9392dcfb0
Eduardo Patrocinio <epatro@gmail.com>
2019-12-19 09:48:27 -0500

    Sleep 5 seconds

--- test-resilience/server.js
+++ test-resilience/server.js
@@ -130,8 +130,8 @@ async function run() {
 	await countMessages(0);
 	await addMessages();
 	await waitForEmptyQueue();
-	console.log ("Sleeping 2 seconds");
-	sleep.sleep(2);
+	console.log ("Sleeping 5 seconds");
+	sleep.sleep(5);
 	await countMessages(NUM_MESSAGES);
 }
 
